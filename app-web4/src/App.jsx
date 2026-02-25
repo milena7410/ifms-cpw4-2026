7e024b6222ecdf6175b0721ae279573ec323ea84
milena7410 <milenaalegre99@gmail.com>
2026-02-25 18:30:21 -0400

    atividade01

---
+++ app-web4/src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div>
+      <h1>Tarefas Diarias</h1>
+      <p>Organize suas tarefas</p>
+      <button>Começar</button>
+    </div>
+  )
+}
+
+export default App
